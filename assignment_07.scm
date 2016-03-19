e7ae804d3d32a2ceb74d4e8c8349f068865a0a0f
trittimo <michaeltrittin@gmail.com>
2016-03-19 16:21:25 -0400

    vector-append-list works

--- assignment_07.scm
+++ assignment_07.scm
@@ -4,9 +4,10 @@
 	(if (< k len)
 		(begin
 			(vector-set! v k (refproc k))
-			(vector-copy v (add1 k) refproc))))
+			(vector-copy v (add1 k) len refproc))))
 
 (define (vector-append-list v ls)
 	(let ([newv (make-vector (+ (length ls) (vector-length v)))])
 		(vector-copy newv 0 (vector-length v) (lambda (k) (vector-ref v k)))
-		(vector-copy newv (vector-length v) (vector-length newv) (lambda (k) (list-ref ls (- k (vector-length v)))))))
+		(vector-copy newv (vector-length v) (vector-length newv) (lambda (k) (list-ref ls (- k (vector-length v)))))
+		newv))
