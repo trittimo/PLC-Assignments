b9d0915f923a6ef7682d6c71494dfe13b084796d
trittimo <michaeltrittin@gmail.com>
2016-03-19 16:13:09 -0400

    Start of assignment 07 and a gitignore file so I can put my test cases in

---
+++ assignment_07.scm
@@ -0,0 +1,12 @@
+; copies into a vector, starting at index k and going until k = len-1, 
+; and using (refproc k) to get the element to insert at index k
+(define (vector-copy v k len refproc)
+	(if (< k len)
+		(begin
+			(vector-set! v k (refproc k))
+			(vector-copy v (add1 k) refproc))))
+
+(define (vector-append-list v ls)
+	(let ([newv (make-vector (+ (length ls) (vector-length v)))])
+		(vector-copy newv 0 (vector-length v) (lambda (k) (vector-ref v k)))
+		(vector-copy newv (vector-length v) (vector-length newv) (lambda (k) (list-ref ls (- k (vector-length v)))))))
